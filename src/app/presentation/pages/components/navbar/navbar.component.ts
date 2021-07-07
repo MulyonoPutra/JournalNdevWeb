46c2b1305716e7dea388be45412b2122e9dbd700
Mulyono Putra <mulyonoputra1994@gmail.com>
2021-07-07 15:22:36 +0700

    Create Navbar Component.

---
+++ src/app/presentation/pages/components/navbar/navbar.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { TokenService } from 'src/app/core/service/token.service';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss'],
+})
+export class NavbarComponent implements OnInit {
+
+  isLogged = false;
+
+  constructor(private tokenService: TokenService) {}
+
+  ngOnInit() {
+    if (this.tokenService.getToken()) {
+      this.isLogged = true;
+    } else {
+      this.isLogged = false;
+    }
+  }
+
+  onLogOut(): void {
+    this.tokenService.logOut();
+    window.location.reload();
+  }
+  
+}

ea4b9cb317f7280449fa6241d4cfcf65cdaaebe5
Mulyono Putra <mulyonoputra1994@gmail.com>
2021-07-09 23:58:16 +0700

    Add assets images.

---
+++ src/app/core/service/utils/utility.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { TokenService } from '../token.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UtilityService {
+
+  isLogged = false;
+
+  constructor(
+    private spinner: NgxSpinnerService,
+    private tokenService: TokenService
+  ) {}
+
+  /* Function to set spinner when navigate to other page */
+  setSpinner() {
+    this.spinner.show();
+    setTimeout(() => {
+      this.spinner.hide();
+    }, 500);
+  }
+
+  checkUserToken() {
+    if (this.tokenService.getToken()) {
+      this.isLogged = true;
+    } else {
+      this.isLogged = false;
+    }
+  }
+}

50860cd52f4188e180b5a5812a92644c3b69d80d
Mulyono Putra <mulyonoputra1994@gmail.com>
2021-07-09 10:55:35 +0700

    Generate Cards new, details components.

---
+++ src/app/presentation/pages/folding-card/details/folding-card-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-folding-card-details',
+  templateUrl: './folding-card-details.component.html',
+  styleUrls: ['./folding-card-details.component.scss']
+})
+export class FoldingCardDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

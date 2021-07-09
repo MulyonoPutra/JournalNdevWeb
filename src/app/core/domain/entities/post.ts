4d37baa2ae1c952170e2d42a642bb2d387c25c7b
Mulyono Putra <mulyonoputra1994@gmail.com>
2021-07-10 00:04:29 +0700

    Add author entity,

--- src/app/core/domain/entities/post.ts
+++ src/app/core/domain/entities/post.ts
@@ -4,9 +4,11 @@ export interface Post {
   id?: number;
   title?: string;
   content?: string;
+  author?: string;
   imagesContentType?: string;
   images?: string;
   category_post?: Category | null;
+  createdDate?: string;
 }
 
 export class IPost implements Post {
@@ -14,17 +16,10 @@ export class IPost implements Post {
     public id?: number,
     public title?: string,
     public content?: string,
+    public author?: string,
     public imagesContentType?: string,
     public images?: string,
-    public category_post?: Category | null
+    public category_post?: Category | null,
+    public createdDate?: string
   ) {}
-}
-
-export interface PostEntity {
-  id: number;
-  title: string;
-  content: string;
-  imagesContentType?: string;
-  images?: string;
-  category_post?: Category | null;
 }

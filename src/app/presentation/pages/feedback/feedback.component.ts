4c58cd70a817f4b238df151923c87932a008729a
Mulyono Putra <mulyonoputra1994@gmail.com>
2021-07-11 00:16:58 +0700

    Commit several changes.

--- src/app/presentation/pages/feedback/feedback.component.ts
+++ src/app/presentation/pages/feedback/feedback.component.ts
@@ -1,15 +1,30 @@
+import { OnDestroy } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
+import { TokenService } from 'src/app/core/service/token.service';
 
 @Component({
   selector: 'app-feedback',
   templateUrl: './feedback.component.html',
-  styleUrls: ['./feedback.component.scss']
+  styleUrls: ['./feedback.component.scss'],
 })
-export class FeedbackComponent implements OnInit {
+export class FeedbackComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  public isLogged = false;
 
-  ngOnInit(): void {
+  constructor(protected tokenService: TokenService) {}
+
+  ngOnInit(): void {}
+
+  getUserToken(): void {
+    if (this.tokenService.getToken()) {
+      this.isLogged = true;
+    } else {
+      this.isLogged = false;
+    }
+  }
+
+  ngOnDestroy(): void {
+    window.location.reload();
   }
 
 }

b0b3806c3a91c948ff8db4d1d6972d323d08e6a7
Mulyono Putra <mulyonoputra1994@gmail.com>
2021-07-13 20:40:15 +0700

    Rename properties.

--- src/app/core/guards/prod-guard.service.ts
+++ src/app/core/guards/prod-guard.service.ts
@@ -12,23 +12,20 @@ import { TokenService } from '../service/token.service';
 })
 export class ProdGuardService implements CanActivate {
 
-  realRole!: string;
+  currentRole!: string;
 
   constructor(private tokenService: TokenService, private router: Router) {}
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
-    const expectedRol = route.data.expectedRol;
+    const expectedRole = route.data.expectedRole;
     const roles = this.tokenService.getAuthorities();
-    this.realRole = 'user';
+    this.currentRole = 'user';
     roles.forEach((rol) => {
       if (rol === 'ROLE_ADMIN') {
-        this.realRole = 'admin';
+        this.currentRole = 'admin';
       }
     });
-    if (
-      !this.tokenService.getToken() ||
-      expectedRol.indexOf(this.realRole) === -1
-    ) {
+    if (!this.tokenService.getToken() || expectedRole.indexOf(this.currentRole) === -1) {
       this.router.navigate(['/']);
       return false;
     }
